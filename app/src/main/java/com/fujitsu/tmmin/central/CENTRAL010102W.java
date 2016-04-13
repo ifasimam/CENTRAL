ad6fd99481cc47e34b993424ecd71cc209afb2d4
Imam Ibnu Faishal <ifasimam@gmail.com>
2016-04-13 12:48:44 -0700

    First Commit

---
+++ app/src/main/java/com/fujitsu/tmmin/central/CENTRAL010102W.java
@@ -0,0 +1,13 @@
+package com.fujitsu.tmmin.central;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CENTRAL010102W extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_central010102w);
+    }
+}
